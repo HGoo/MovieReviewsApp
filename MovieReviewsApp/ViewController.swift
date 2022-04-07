40ad1669c8e9cf89d89ed0ff3b9b266daa94faae
Nikolay <halemhood@icloud.com>
2022-04-07 16:02:30 +0300

    Initial Commit

---
+++ MovieReviewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieReviewsApp
+//
+//  Created by Николай Петров on 07.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
