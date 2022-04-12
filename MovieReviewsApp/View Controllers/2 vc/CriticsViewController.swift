6075c2d6a275caa469c7ea4251e9484ec85ceb71
Nikolay <halemhood@icloud.com>
2022-04-12 20:15:11 +0300

    fix commit

--- MovieReviewsApp/View Controllers/2 vc/CriticsViewController.swift
+++ MovieReviewsApp/View Controllers/2 vc/CriticsViewController.swift
@@ -8,23 +8,74 @@
 import UIKit
 
 class CriticsViewController: UIViewController {
+    
+    private let criticsJsonUrl = "https://api.nytimes.com/svc/movies/v2/critics/all.json?api-key=GW5a0tJfWOcfQ7k3dpQizIsrmpZ33Bmm"
+    private var critics: Critics?
 
+    @IBOutlet var collectionViewCritics: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        collectionViewCritics.delegate = self
+        collectionViewCritics.dataSource = self
+
+        fetchData()
+    }
+    
+    func fetchData() {
+        guard let url = URL(string: criticsJsonUrl) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, _ in
+            guard let data = data else { return }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                self.critics = try decoder.decode(Critics.self, from: data)
+                DispatchQueue.main.async { [weak self] in
+                    guard let self = self else { return }
+                    self.collectionViewCritics.reloadData()
+                }
+                print(self.critics ?? "Error review")
+            } catch let error {
+                print(error)
+            }
+        }.resume()
     }
     
 
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+extension CriticsViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return critics?.results?.count ?? 0
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellCritics", for: indexPath) as! CriticViewCell
+
+
+        guard let critic = critics?.results?[indexPath.row] else { return cell }
+
+        cell.configureCritic(with: critic)
+
+        //cell.layoutIfNeeded()
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let size = view.frame.size.width / 2
+        return CGSize(width: size - 30, height: size + 50)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 30
+    }
+    
+    
+}
+
